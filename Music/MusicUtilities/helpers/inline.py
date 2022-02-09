8ad05da433eff4c44011ec8cd5648ced013fc133
xsyn1100 <98083245+xsyn1100@users.noreply.github.com>
2022-02-09 19:00:44 +0700

    Update inline.py

--- Music/MusicUtilities/helpers/inline.py
+++ Music/MusicUtilities/helpers/inline.py
@@ -12,7 +12,7 @@ def play_markup(videoid, user_id):
     buttons= [
             [      
                 InlineKeyboardButton(text="• Menu", callback_data=f'other {videoid}|{user_id}'),
-                InlineKeyboardButton(text="• Close", callback_data=f'close2')
+                InlineKeyboardButton(text="🗑 Close", callback_data=f'close2')
             ],
         ]
     return buttons 
@@ -98,7 +98,7 @@ def search_markup(ID1, ID2, ID3, ID4, ID5, duration1, duration2, duration3, dura
             [ 
                 
                 InlineKeyboardButton(text="⬅️", callback_data=f'popat 1|{query}|{user_id}'), 
-                InlineKeyboardButton(text="❌​", callback_data=f"ppcl2 smex|{user_id}") ,
+                InlineKeyboardButton(text="🗑​", callback_data=f"ppcl2 smex|{user_id}") ,
                 InlineKeyboardButton(text="➡️", callback_data=f'popat 1|{query}|{user_id}')             
             ],
         ]
@@ -118,7 +118,7 @@ def search_markup2(ID6, ID7, ID8, ID9, ID10, duration6, duration7, duration8, du
             [ 
                 
                 InlineKeyboardButton(text="⬅️", callback_data=f'popat 2|{query}|{user_id}'), 
-                InlineKeyboardButton(text="❌", callback_data=f"ppcl2 smex|{user_id}") ,
+                InlineKeyboardButton(text="🗑", callback_data=f"ppcl2 smex|{user_id}") ,
                 InlineKeyboardButton(text="➡️", callback_data=f'popat 2|{query}|{user_id}')             
             ],
         ]
