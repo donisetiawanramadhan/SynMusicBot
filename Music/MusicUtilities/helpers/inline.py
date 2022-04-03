0b1c7ccfaf9cd4cd546e4d939ab0a0e594795531
Doni Ramadhan <98083245+xsyn1100@users.noreply.github.com>
2022-04-03 21:50:51 +0700

    Update inline.py

--- Music/MusicUtilities/helpers/inline.py
+++ Music/MusicUtilities/helpers/inline.py
@@ -99,7 +99,7 @@ def search_markup(ID1, ID2, ID3, ID4, ID5, duration1, duration2, duration3, dura
                 InlineKeyboardButton(text="➡️", callback_data=f'popat 1|{query}|{user_id}')
             ],
             [
-                InlineKeyboardButton(text="••ᴄʟᴏsᴇ••", callback_data=f"ppcl2 smex|{user_id}")
+                InlineKeyboardButton(text="ᴄʟᴏsᴇ", callback_data=f"ppcl2 smex|{user_id}")
             ],
         ]
     return buttons   
@@ -120,7 +120,7 @@ def search_markup2(ID6, ID7, ID8, ID9, ID10, duration6, duration7, duration8, du
                 InlineKeyboardButton(text="⬅️", callback_data=f'popat 2|{query}|{user_id}')
             ],
             [
-                InlineKeyboardButton(text="••ᴄʟᴏsᴇ••", callback_data=f"ppcl2 smex|{user_id}")
+                InlineKeyboardButton(text="ᴄʟᴏsᴇ", callback_data=f"ppcl2 smex|{user_id}")
             ],
         ]
     return buttons
